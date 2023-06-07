a1a7ac52d17ab78acc0a7c1386b36921861789c1
franko <fstipanov@gmail.com>
2023-06-07 13:21:01 +0200

    base problem

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "//sidebar-modal-context-API/",
+  base: "/sidebar-modal-context-API/",
   plugins: [react()],
 })
